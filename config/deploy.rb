aa3f54312d8ecf022aa511680403ca9d43e0d093
Camron Frenzel <camron.frenzel@bleepworks.com>
2015-12-03 13:37:34 -0500

    second

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'railsgirls'
+set :repo_url, 'http://github.com/cfrenzel/cap_temp.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
